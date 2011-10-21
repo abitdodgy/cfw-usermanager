4a803f7b07c18efe114bbd59279a8813092c1352
Mohamad El-Husseini <husseini.mel@gmail.com>
2011-10-21 08:27:32 -0300

    adding missing files

---
+++ cfw-usermanager.sql
@@ -0,0 +1,69 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : Default
+Source Server Version : 50515
+Source Host           : localhost:3306
+Source Database       : cfw-usermanager
+
+Target Server Type    : MYSQL
+Target Server Version : 50515
+File Encoding         : 65001
+
+Date: 2011-10-21 08:12:39
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `roles`
+-- ----------------------------
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE `roles` (
+  `id` tinyint(2) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `description` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of roles
+-- ----------------------------
+INSERT INTO `roles` VALUES ('1', 'User', null);
+INSERT INTO `roles` VALUES ('2', 'Moderator', null);
+INSERT INTO `roles` VALUES ('3', 'Admin', null);
+
+-- ----------------------------
+-- Table structure for `tokens`
+-- ----------------------------
+DROP TABLE IF EXISTS `tokens`;
+CREATE TABLE `tokens` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL,
+  `token` varchar(36) NOT NULL,
+  `expires` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tokens
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `users`
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `salt` varchar(36) NOT NULL,
+  `roleid` tinyint(2) NOT NULL,
+  `confirmed` enum('1','0') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
